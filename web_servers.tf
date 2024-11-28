70a8489e34aec8a6a25c04d327d2723438e69d8a
aazh <strrazv@gmail.com>
2024-11-28 19:38:35 +0200

    28.11

--- web_servers.tf
+++ web_servers.tf
@@ -1,34 +1,26 @@
 resource "yandex_compute_instance" "web_server" {
-  count       = 2
-  name        = "web-server-${count.index}"
-  hostname    = "web-server-${count.index}"
-  zone        = "ru-central1-a"
-  platform_id = "standard-v1"
+  count = 2
+  name  = "web-server-${count.index}"
+
+  allow_stopping_for_update = true
 
   resources {
     cores         = 2
     memory        = 2
-    core_fraction = 20
+    core_fraction = 100
   }
 
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.id
-      type     = "network-hdd"
     }
   }
 
   network_interface {
     subnet_id = yandex_vpc_subnet.private.id
-    nat       = false
   }
 
   metadata = {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-    user-data = <<-EOF
-      #cloud-config
-      hostname: web-server-${count.index}
-      fqdn: web-server-${count.index}.ru-central1.internal
-    EOF
   }
 }
